ad0c85b05c17584b90e54d2c7da62e75710d5b9f
Nizam Uddin <nizam.icup@gmail.com>
2016-12-12 21:47:57 +0100

    Bank part of the project is completed

---
+++ src/Bank/RejectedException.java
@@ -0,0 +1,13 @@
+package Bank;
+
+final public class RejectedException extends Exception {
+    private static final long serialVersionUID = 5306521076696198128L;
+
+    public RejectedException(String reason) {
+        super(reason);
+    }
+
+    public RejectedException(String reason, Throwable rootCause) {
+        super(reason, rootCause);
+    }
+}
